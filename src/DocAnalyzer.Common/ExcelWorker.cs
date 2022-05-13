6ec6cf74bdff0466a33a686f36274d64eb8621d3
Viktor Shevchenko <kaspianturbo@gmail.com>
2022-05-13 08:00:19 +0300

    init proj

---
+++ src/DocAnalyzer.Common/ExcelWorker.cs
@@ -0,0 +1,14 @@
+﻿using OfficeOpenXml;
+
+namespace DocAnalyzer.Common
+{
+    internal class ExcelWorker
+    {
+        public ExcelWorker()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var file = new FileInfo(@"C:\Users\Viktor\Desktop\DocAnalyzer\073 МВКД  ДФ 1 курс - акр 2022.xls");
+
+        }
+    }
+}
